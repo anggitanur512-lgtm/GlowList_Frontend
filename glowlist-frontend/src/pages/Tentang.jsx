38630cf89788bbb7cfcb0a748a18d76108642326
anggitanur512-lgtm <anggitanur512@gmail.com>
2026-08-05 12:30:47 +0700

    Update Tentang.jsx

--- glowlist-frontend/src/pages/Tentang.jsx
+++ glowlist-frontend/src/pages/Tentang.jsx
@@ -1,3 +1,17 @@
 export default function Tentang() {
-    return <h2>halaman tentang</h2>;
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Tentang Saya</h2>
+
+      <p><strong>Nama:</strong>Anggita Nur Hanifah</p>
+      <p><strong>Kelas:</strong> XII RPL</p>
+
+      <h3>Alasan Suka Skincare</h3>
+      <p>
+        Saya suka skincare karena dapat membantu menjaga kesehatan kulit,
+        membuat wajah tetap bersih, dan meningkatkan rasa percaya diri.
+        Dengan perawatan yang rutin, kulit terasa lebih segar dan terawat.
+      </p>
+    </div>
+  );
 }
