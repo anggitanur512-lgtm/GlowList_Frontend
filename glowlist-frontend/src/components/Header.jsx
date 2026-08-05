e16a70b3985c216915dd338108aef05785253b82
anggitanur512-lgtm <anggitanur512@gmail.com>
2026-08-05 14:14:13 +0700

    produk 1

--- glowlist-frontend/src/components/Header.jsx
+++ glowlist-frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header(){
     return (
-        <nav className="navbar navbar-drak bg-dark px-3">
+        <nav className="navbar navbar-dark bg-dark px-3">
             <Link to="/" className="nevbar-brand"> GlowList</Link>
             <button className="btn btn-danger">Logout</button>
         </nav>
