a0165b99ef91fd78eef2065d9dbc02987afee5a6
anggitanur512-lgtm <anggitanur512@gmail.com>
2026-08-05 14:45:33 +0700

    Update Kategori.jsx

--- glowlist-frontend/src/pages/Kategori.jsx
+++ glowlist-frontend/src/pages/Kategori.jsx
@@ -1,3 +1,59 @@
-export default function Produk() {
-    return <h2>Halaman Kategori</h2>;
+import { useEffect, useState  } from "react";
+
+export default function Kategori() {
+    const [kategori, setKategori] = useState([]);
+    const [loading, setLoading] = useState ([]);
+
+    const getKategori = async () => {
+        try {
+            const res = await fetch("http:/localhost:5000/kategori");
+            const data = await res.json();
+            setKategori(data);
+        } catch (err) {
+            console.error("Gagal fetch data:", err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getKategori();
+    }, []) 
+
+    if (loading) {
+        return <div className="container mt-4">Sedang memuat data...</div>
+    }
+
+    return (
+        <div className="container mr-4">
+            <div className="d-flex justify-content-between align-items-center mb-3">
+                <h2>Daftar Kategori GlowList ✨</h2>
+            </div>
+
+            <table className="table table-bordered table-striped">
+                <thead className="table-promary">
+                    <tr>
+                        <th>ID</th>
+                        <th>kategori</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {kategori.length > 0 ? (
+                        kategori.map((item) => (
+                            <tr key={item.id_kategori}>
+                                <td>{item.id_kategori}</td>
+                                <td>{item.kategori}</td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan="4" className="text-center">
+                                belum ada kategori
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
 }
