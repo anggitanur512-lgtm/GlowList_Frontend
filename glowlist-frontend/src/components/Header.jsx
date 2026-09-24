b72b2b0b7e60abe50df1d16c243e18270479e6ce
anggitanur512-lgtm <anggitanur512@gmail.com>
2026-09-24 11:36:15 +0700

    Update Header.jsx

--- glowlist-frontend/src/components/Header.jsx
+++ glowlist-frontend/src/components/Header.jsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Header(){
+    // di dalam komponen Header:
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("login")
+    };
+    // pasang di tombol Logout:
+    <button onClick={handleLogout} className="btn btn-danger">Logout</button>
     return (
         <nav className="navbar navbar-dark bg-dark px-3">
             <Link to="/" className="nevbar-brand"> GlowList</Link>
-            <button className="btn btn-danger">Logout</button>
+            <button onClick={handleLogout} className="btn btn-danger">Logout</button>
         </nav>
     );
 }
